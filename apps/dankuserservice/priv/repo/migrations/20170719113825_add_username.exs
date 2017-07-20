760cc650571b2cd1664df9469a97286b8558005b
arkanoryn <arkanoryn@gmail.com>
2017-07-20 14:18:12 +0200

    Rework the user service hierarchy
    * Separate fetcher functions from registration functions
    * Remove Authentication function from the service
    * Add a secret example

--- apps/dankuserservice/priv/repo/migrations/20170719113825_add_username.exs
+++ apps/dankuserservice/priv/repo/migrations/20170719113825_add_username.exs
@@ -5,11 +5,12 @@ defmodule DankUserService.Repo.Migrations.AddUsername do
     create table(:users) do
       add :username, :string, null: false
       add :email, :string, null: false
-      add :github_key, :string
+      add :password_hash, :string, null: false
+      add :github_key, :string, null: false, default: ""
 
       timestamps()
     end
-    
+
     create unique_index :users, :username
     create unique_index :users, :email
   end
