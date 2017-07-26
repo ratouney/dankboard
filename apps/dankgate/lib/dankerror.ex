edeecc2c54a6e1ed693050bba045377a6be58d4c
Jean Pignouf <mfouchardiere@avarteq.de>
2017-07-26 12:09:14 +0000

    * Changed a couple function returns to keep the same syntax * Added documentation module using ExDoc

--- apps/dankgate/lib/dankerror.ex
+++ apps/dankgate/lib/dankerror.ex
@@ -1,15 +1,25 @@
 defmodule Dankgate.Error do
+  @moduledoc """
+    This module is made to create readable errors from variable output
 
-  # Since ecto has a really weird error output, here is a EctoError -> String func
+    For now it's only handling Ecto.Changeset errors and converting them into a single string
+  """
 
+  @doc """
+    Converts an ecto changeset errorset to a error message as a string
+
+    First, we separate the main tag from the message part, for example :
+        iex(1)> [{key, msg}] = {username: ["has already been taken", {}]}
+
+    The key which is an atom in converted to a string and capitalized.
+    The message itself has arguments so it's treatet by the Dankgate.Error.put_values/1 function
+
+    Once this is done, the both parts are fused so the part above would be :
+    ```{username: ["has already been taken", {}]}``` -> ```"Username has already been taken"```
+    
+  """
   def ectoerrs_to_string(error) do
     [{key, val}] = error
-    # Since we have a format like this : 
-    # [errorkey: {"errmsg", [errmsg_arg]}]
-    #
-    # We convert the first error key (if one doesn't work, the others wont either)
-    # from an atom to a Capitalized string
-
     param = 
       key
       |> to_string
@@ -17,15 +27,25 @@ defmodule Dankgate.Error do
     param <> " " <> put_values(val)
   end
 
+  @doc """
+    This function is specificaly made to interpolate the keys from an argument list into a string
 
-  # Great and simple way to format ecto errors 
-  # http://www.thisisnotajoke.com/blog/2015/09/serializing-ecto-changeset-errors-to-jsonapi-in-elixir.html
-  
+    This was applied to Ecto.Changesets here :
+    [http://www.thisisnotajoke.com/blog/2015/09/serializing-ecto-changeset-errors-to-jsonapi-in-elixir.html]
+
+    It takes the values from the argument list next ot the string and applies them by replacing the found key by their values.
+
+    As an example, if the given password for a user creation is too short, an errorset like this would be given :
+
+    -> ```{"should be at least %{count} character(s)", [count: 6, validation: :length, min: 6]}```
+
+    which becomes becomes
+
+    -> ```"should be at leat 6 characters(s)"```
+  """
   def put_values({message, values}) do
-    # first take the whole message as accumulator and the values as modifiers
     Enum.reduce(values, message, 
       fn {key, val}, acc ->
-        # If a key in the values is found in the string, replace it with its value
         String.replace(acc, "%{#{key}}", to_string(val))
       end
     )
